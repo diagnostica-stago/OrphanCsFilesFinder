0416e6ae5b1edd929f2c225b2e2bf3cb3e1135f4
Thomas Radioyes <thomas.radioyes@stago.com>
2020-07-23 10:57:15 +0200

    First public version

---
+++ OrphanCsFileFinder/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using McMaster.Extensions.CommandLineUtils;
+
+namespace OrphanCsFileFinder
+{
+    public class Program
+    {
+        [Argument(0, Description = "The directory to analyze.")]
+        [Required]
+        public string TopDirectory { get; } = string.Empty;
+
+        [Option("-g|--include-generated" , CommandOptionType.NoValue , Description = "Include .generated.cs files in the orphaned files")]
+        public bool IncludeGeneratedFiles { get; }
+
+        public static void Main(string[] args)
+        {
+            CommandLineApplication.Execute<Program>(args);
+        }
+
+        private ValidationResult OnValidate()
+        {
+            if (!Directory.Exists(TopDirectory))
+            {
+                return new ValidationResult("Directory does not exist");
+            }
+
+            return ValidationResult.Success;
+        }
+
+        public void OnExecute()
+        {
+            var directoriesToCheck = new Queue<DirectoryInfo>();
+            directoriesToCheck.Enqueue(new DirectoryInfo(TopDirectory));
+            var orphanFiles = new List<string>();
+            while (directoriesToCheck.TryDequeue(out var currentDirectory))
+            {
+                if (currentDirectory.EnumerateFiles("*.csproj").Any())
+                {
+                    continue;
+                }
+
+                var csFiles = currentDirectory
+                    .EnumerateFiles("*.cs")
+                    .Where(f => IncludeGeneratedFiles || !f.Name.EndsWith(".Generated.cs", StringComparison.InvariantCultureIgnoreCase))
+                    .Select(f => f.FullName);
+                orphanFiles.AddRange(csFiles);
+
+                foreach (var directoryInfo in currentDirectory.EnumerateDirectories())
+                {
+                    directoriesToCheck.Enqueue(directoryInfo);
+                }
+            }
+
+            if (orphanFiles.Any())
+            {
+                Console.WriteLine("Orphaned cs files:");
+            }
+            foreach (var orphanFile in orphanFiles)
+            {
+                Console.WriteLine(orphanFile);
+            }
+            Console.WriteLine("Done");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# OrphanCsFilesFinder
+# OrphanCsFileFinder
 Finds .cs files without a csproj in a directory over them
+
+### Installation
+
+```
+dotnet tool install -g orphancsfilesfinder
+```
+
+### Usage
+
+```
+orphancsfilesfinder <TopDirectory>
+```
+
+Arguments:
+- TopDirectory            The directory to analyze.
+
+Options:
+  - `-g|--include-generated` : Include .generated.cs files in the orphaned files
+  - `-?|-h|--help` : Show help information
